4df39dd68079b71565af0ed4c598ad763e644397
Vasily <vasalekmas@gmail.com>
2020-04-29 16:02:23 +0300

    Servlet. Web.xml [#6866]

---
+++ src/main/java/ru/job4j/controller/PostServlet.java
@@ -0,0 +1,21 @@
+package ru.job4j.controller;
+
+import ru.job4j.model.Post;
+import ru.job4j.model.Store;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class PostServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        Store.instOf().save(new Post(0, req.getParameter("vacancy")));
+        resp.sendRedirect(req.getContextPath() + "/posts.jsp");
+    }
+}
